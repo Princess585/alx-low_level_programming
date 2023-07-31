ea9b20855313698e5d37dfd7651ef3c9344f6169
Princess585 <princessdkhombisa1994@gmail.com>
2023-07-31 05:37:09 -0700

    Task 0 and 1

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - A function that fills memory with a constant byte
+ * @s: An area to fill with bytes
+ * @b: A byte that is constant
+ * @n: Bytes number
+ * Return: pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
